f95e568245db195abf4aaddf060b13e7a8597d8e
Mohammed Khan <makhan52@neiu.edu>
2022-02-11 23:53:23 -0600

    Resolved the problems for HW

--- OOP Review/Inheritance/src/Square.java
+++ OOP Review/Inheritance/src/Square.java
@@ -2,3 +2,39 @@
  Add variables if you need.
  Remember to use the @Override to override methods.
 intelliJ will be helpful if you do).*/
+
+public class Square extends Shape {
+    double lengthSide;
+
+    public Square(double lengthSide, String name) {
+        super(name);
+        this.lengthSide = lengthSide;
+    }
+
+    @Override
+    public double getArea() {
+        double area = this.lengthSide * this.lengthSide;
+        return area;
+    }
+
+    @Override
+    public double getPerimeter() {
+        double perimeter = this.lengthSide * 4;
+        return perimeter;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null && obj.getClass() == this.getClass()) {
+            Square s = (Square) obj;
+            return this.name.equals(s.name) && this.lengthSide == (s.lengthSide);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        String str = this.name + ":" + this.lengthSide;
+        return str;
+    }
+}
--- OOP Review/Polymorphism/src/AdultPatient.java
+++ OOP Review/Polymorphism/src/AdultPatient.java
@@ -1,5 +1,52 @@
 import java.util.ArrayList;
 
-public class AdultPatient/* TODO */
-Implement the class Adult Patient. 
-An adult patient is a Person that is Diagnosable and that is Employable.
+public class AdultPatient extends Person implements Diagnosable,Employable{
+    double temperature;
+    int systolic;
+    int diastolic;
+    ArrayList<String> previousjobs;
+
+    @Override
+    public double getTemperature() {
+
+        return temperature;
+    }
+    @Override
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public int getSystolic() {
+
+        return systolic;
+    }
+    @Override
+    public int getDiastolic() {
+
+        return diastolic;
+    }
+
+    @Override
+    public void setSystolic(int systolic) {
+
+        this.systolic = systolic;
+    }
+
+    @Override
+    public void setDiastolic(int diastolic) {
+
+        this.diastolic = diastolic;
+    }
+    @Override
+    public void addJob(String job) {
+        if(this.previousjobs== null)
+            this.previousjobs = new ArrayList<String>();
+        this.previousjobs.add(job);
+    }
+
+    @Override
+    public ArrayList<String> getPreviousJobs(){
+        return this.previousjobs;
+    }
+}
